3020f21ad6daeaa67ecab7292428a18923c856d9
max91246 <max91246@gmail.com>
2020-09-18 16:00:31 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Laravel Livewire Demo</title>
+    <!-- Styles -->
+    <livewire:styles />
+</head>
+<body>
+<livewire:counter />
+<livewire:scripts />
+</body>
+</html>
